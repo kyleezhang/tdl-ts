61f8af51954438eb830e73b0765d1a8606592337
zhangtao.kylee <zhangtao.kylee@bytedance.com>
2022-05-08 11:22:06 +0800

    :sparkles: add 18-easy-tuple-length & 43-easy-exclude

--- type-challenges/14-easy-first/index.ts
+++ type-challenges/14-easy-first/index.ts
@@ -1,1 +1,8 @@
-export type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+// 解法1
+// export type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+
+// 解法2
+// export type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
+
+// 解法3
+export type First<T extends any[]> = T extends [infer P, ...infer Rest] ? P : never
