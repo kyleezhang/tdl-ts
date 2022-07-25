9d1c624ae2bcbf655867a092c0ae0cc90e13e0be
kyleezhang <2942181559@qq.com>
2022-07-26 00:01:16 +0800

    :sparkles: 每日浅刷一下type-challenges

---
+++ type-challenges/medium/4803-medium-trim-right/test.ts
@@ -0,0 +1,12 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import type { TrimRight } from './index'
+
+type cases = [
+  Expect<Equal<TrimRight<'str'>, 'str'>>,
+  Expect<Equal<TrimRight<'str '>, 'str'>>,
+  Expect<Equal<TrimRight<'str     '>, 'str'>>,
+  Expect<Equal<TrimRight<'     str     '>, '     str'>>,
+  Expect<Equal<TrimRight<'   foo bar  \n\t '>, '   foo bar'>>,
+  Expect<Equal<TrimRight<''>, ''>>,
+  Expect<Equal<TrimRight<'\n\t '>, ''>>,
+]
